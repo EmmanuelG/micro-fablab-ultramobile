d4e10d11b2446a45eaa24accad63270ef5035683
Emmanuel Gilloz <emmanuel.gilloz@gmail.com>
2012-10-29 18:28:10 +0100

    pushing my repo to github

---
+++ micro-fablab-ultra-mobile.mm
@@ -0,0 +1,61 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1330434063057" ID="Freemind_Link_1179413741" MODIFIED="1332273323359" TEXT="Micro-Lab (micro-fablab-ultra-mobile)">
+<node CREATED="1330434292278" ID="Freemind_Link_90963882" MODIFIED="1330434294658" POSITION="right" TEXT="ensgsi"/>
+<node CREATED="1330434294929" ID="Freemind_Link_891848778" MODIFIED="1330434297878" POSITION="right" TEXT="FING"/>
+<node CREATED="1330436096048" ID="Freemind_Link_199789127" MODIFIED="1330436102116" POSITION="right" TEXT="OpenSourceEcology"/>
+<node CREATED="1330436076595" ID="Freemind_Link_1416544982" MODIFIED="1330436082424" POSITION="right" TEXT="imagination for people"/>
+<node CREATED="1330436082864" ID="Freemind_Link_618041237" MODIFIED="1330436087781" POSITION="right" TEXT="innovafrica">
+<node CREATED="1330435525268" ID="Freemind_Link_1639747668" MODIFIED="1330435528698" TEXT="centre songhai"/>
+<node CREATED="1330436064373" ID="Freemind_Link_1601574394" MODIFIED="1330436068327" TEXT="fondation Zinsou"/>
+<node CREATED="1330434104677" ID="_" MODIFIED="1330434109173" TEXT="croix-rouge">
+<node CREATED="1332273328426" ID="Freemind_Link_59791793" MODIFIED="1332273334433" TEXT="casques-rouges"/>
+</node>
+</node>
+<node CREATED="1330435528900" ID="Freemind_Link_1316994782" MODIFIED="1330435916657" POSITION="right" TEXT="centre pompidou"/>
+<node CREATED="1330435946073" ID="Freemind_Link_1314107716" MODIFIED="1330435949677" POSITION="right" TEXT="fablabs">
+<node CREATED="1332144241430" ID="Freemind_Link_1405800723" MODIFIED="1332144245714" TEXT="faclab"/>
+<node CREATED="1332144246087" ID="Freemind_Link_1343029042" MODIFIED="1332144246769" TEXT="et"/>
+<node CREATED="1332144247155" ID="Freemind_Link_41901707" MODIFIED="1332144247626" TEXT="al"/>
+</node>
+<node CREATED="1330468705454" ID="Freemind_Link_1084741830" MODIFIED="1330468707830" POSITION="right" TEXT="NodA"/>
+<node CREATED="1331506261464" ID="Freemind_Link_1440192223" MODIFIED="1331506263911" POSITION="left" TEXT="financement">
+<node CREATED="1330436046891" ID="Freemind_Link_1272545508" MODIFIED="1330436050955" TEXT="salariat ?"/>
+<node CREATED="1330436019072" ID="Freemind_Link_1161715696" MODIFIED="1333925804449" TEXT="m&#xe9;c&#xe9;nat"/>
+<node CREATED="1330436010347" ID="Freemind_Link_371569309" MODIFIED="1330436015449" TEXT="crowdsoucing">
+<node CREATED="1332144214797" ID="Freemind_Link_269832298" MODIFIED="1332144216542" TEXT="ulule"/>
+<node CREATED="1348968344690" ID="Freemind_Link_1634384248" MODIFIED="1348968347252" TEXT="goteo"/>
+</node>
+</node>
+<node CREATED="1331506270177" ID="Freemind_Link_1328209941" MODIFIED="1332144130033" POSITION="left" TEXT="1m^3 d&apos;&#xe9;quipement">
+<node CREATED="1331506275271" ID="Freemind_Link_1659654758" MODIFIED="1333925740814" TEXT="FoldaRap">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1332144196797" ID="Freemind_Link_85982941" MODIFIED="1332144200556" TEXT="graveuse laser de poche"/>
+<node CREATED="1331506277166" ID="Freemind_Link_1666589249" MODIFIED="1331506279196" TEXT="mini cnc"/>
+<node CREATED="1331506295642" ID="Freemind_Link_1950572723" MODIFIED="1331506298105" TEXT="plotter"/>
+<node CREATED="1331506299917" ID="Freemind_Link_1506993629" MODIFIED="1336169239936" TEXT="imprimante portable">
+<node CREATED="1336169260691" ID="Freemind_Link_1343956982" LINK="http://www.amazon.fr/Canon-Imprimante-portable-batterie-PictBridge/dp/B0017VKPW4/ref=sr_1_3?s=computers&amp;ie=UTF8&amp;qid=1336168929&amp;sr=1-3" MODIFIED="1336169281138" TEXT="Pixma IP100">
+<node CREATED="1342273415331" ID="Freemind_Link_1112022850" MODIFIED="1342273417828" TEXT="250 &#x20ac;"/>
+</node>
+<node CREATED="1336169293844" ID="Freemind_Link_920782784" LINK="http://www.amazon.fr/Canon-BJC-80-Imprimante-couleur-feuilles/dp/B000178XYM/ref=sr_1_7?s=computers&amp;ie=UTF8&amp;qid=1336168929&amp;sr=1-7" MODIFIED="1336169300715" TEXT="Canon BJC-80">
+<node CREATED="1342273422305" ID="Freemind_Link_1387960646" MODIFIED="1342273424535" TEXT="150 &#x20ac;"/>
+</node>
+<node CREATED="1336169311179" ID="Freemind_Link_474760090" LINK="http://www.amazon.fr/Brother-PocketJet-Imprimante-thermique-infrarouge/dp/B004UCA32E/ref=sr_1_14?s=computers&amp;ie=UTF8&amp;qid=1336168929&amp;sr=1-14" MODIFIED="1342273341795" TEXT="PocketJet 6 PJ-622">
+<node CREATED="1342273343055" ID="Freemind_Link_917269276" MODIFIED="1342273420160" TEXT="400-600 &#x20ac;"/>
+</node>
+</node>
+<node CREATED="1336169240581" FOLDED="true" ID="Freemind_Link_1495940231" MODIFIED="1336169242937" TEXT="scanner portable">
+<node CREATED="1336169355447" ID="Freemind_Link_34921704" LINK="http://www.amazon.fr/Iris-Computer-USOA447-Can-Express/dp/B000V9OD1C/ref=sr_1_23?s=computers&amp;ie=UTF8&amp;qid=1336168929&amp;sr=1-23" MODIFIED="1336169363630" TEXT="Iris Can"/>
+</node>
+<node CREATED="1331506279699" ID="Freemind_Link_605602097" MODIFIED="1331506280823" TEXT="kinect"/>
+<node CREATED="1331506281358" ID="Freemind_Link_998684572" MODIFIED="1333925777202" TEXT="mini-d&#xe9;coupe-laser"/>
+<node CREATED="1331506284583" ID="Freemind_Link_1512925697" MODIFIED="1331506288542" TEXT="arducopter"/>
+<node CREATED="1338410908504" ID="Freemind_Link_1929949859" MODIFIED="1338410910721" TEXT="multimedia">
+<node CREATED="1331509145704" ID="Freemind_Link_963652370" MODIFIED="1331509150135" TEXT="pico-projecteur"/>
+<node CREATED="1338410914398" ID="Freemind_Link_1652255184" MODIFIED="1338410918018" TEXT="enceinte &#xe0; r&#xe9;sonance"/>
+</node>
+<node CREATED="1331509480412" ID="Freemind_Link_228452300" MODIFIED="1331509482866" TEXT="pirate-box"/>
+</node>
+</node>
+</map>
